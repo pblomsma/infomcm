5959efcf2303974670001636e15e1d061cfd15d9
DESKTOP-4SG5G2D\adria <adrian.briceno.aguilar@gmail.com>
2017-12-01 10:21:15 +0100

    no message

--- Assignment_1/question_3.R
+++ Assignment_1/question_3.R
@@ -1,5 +1,11 @@
-COG_DATA = read.csv("data\\keyPressDataWithLaneDeviation.csv", sep=",")
+COG_DATA = read.csv("C:\\Users\\adria\\OneDrive\\Documents\\keyPressDataWithLaneDeviation.csv", sep=",")
 no_typing_errors <- COG_DATA[COG_DATA$typingErrorMadeOnTrial == 0,]
+# Part A
+singleDialing2 <- no_typing_errors[no_typing_errors$partOfExperiment == "singleDialing2",]
+singleDialing2_11 <- singleDialing2[singleDialing2$phoneNrLengthBeforeKeyPress== 11,]
+singleDialing2_11$meaninterval <- singleDialing2_11$timeRelativeToTrialStart/12
+mean(singleDialing2_11$meaninterval)
+# Part B
 
 #Calculate for each participant what the average interkeypressinterval is between two digits in 
 #the single -  task trial data that was collected at the end of the experiment (partOfExperiment == "
