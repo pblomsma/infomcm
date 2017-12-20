512dd4b361525fedcb47568279db08a12467b1cc
Peter Blomsma <peter.blomsma@gmail.com>
2017-12-20 11:13:37 +0100

    First version of the preprocess function.

--- Assignment_2/intro.R
+++ Assignment_2/intro.R
@@ -38,6 +38,8 @@ classifyText <- function(text, model)
 #Main code:
 model <- trainModel(data.train)
 
+
+#Step 1: Create the word probabilities
 trainModel <- function(traindata)  
 {
   #P(w_i|c = 1) = (freq w_i|c = 1) / total freq w_i
@@ -54,10 +56,41 @@ trainModel <- function(traindata)
   
   return(traindata)
 }
+
+#Step 2: Preprocess the text
+preprocess <- function(text, model)  
+{
+  library(tokenizers)
+  
+  #Download the lists.
+  common.words <- scan("http://ricknouwen.org/stopwords.txt",sep="\n", what="")
+  sentiment.words <- scan("http://ricknouwen.org/sentimentwords.txt", sep="\n", what="")
+  adjective.words <-    scan("http://ricknouwen.org/adjectives.txt", sep="\n", what="")
+  
+  #tmp
+  text <- data.test[23,]$txt
+  
+  #convert text to vector
+  text <- unlist(tokenize_words(text))
+                 
+  #only known words.
+  unknownwords <-  setdiff(text, model$term)
+  text <- intersect(text ,model$term)
+  
+  #Substract the other lists from the text
+  text <- setdiff(text,common.words)
+  text <- setdiff(text,adjective.words)
+  
+  #only sentimental words.
+  text <- intersect(text, sentiment.words)
+  
+  return(text)
+}
+
 classifier <- function(text.model){
-clas <- integer()
-pos_words <- text.model[text.model$sentiment == 1,]
-neg_words <- text.model[text.model$sentiment == 0,]
+  clas <- integer()
+  pos_words <- text.model[text.model$sentiment == 1,]
+  neg_words <- text.model[text.model$sentiment == 0,]
   posmulti <- 1
   negmulti <- 1
   prob_class <- 0.5
@@ -71,12 +104,12 @@ neg_words <- text.model[text.model$sentiment == 0,]
     negmulti <- negmulti * neg_words$prob[i]
   }  
   clasneg <- prob_class*negmulti
-
+  
   if(clasneg<claspos){
     clas <- 1
   } else 
     clas <- 0
   return(clas)
 }    
-  
+
 classifier(model)
